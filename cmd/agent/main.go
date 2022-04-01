484fcb23b853582573e0da178275f96e9ee7ee0b
AlekseyKas <gos7Pride@yandex.ru>
2022-04-01 11:42:31 +0300

    first commit incriment-3

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,3 +1,179 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"os"
+	"os/signal"
+	"reflect"
+	"runtime"
+	"strings"
+	"syscall"
+	"time"
+
+	"github.com/fatih/structs"
+	"github.com/go-resty/resty/v2"
+	"github.com/sirupsen/logrus"
+)
+
+//init typs
+type gauge float64
+type counter int64
+
+//delay
+const pollInterval = 2 * time.Second
+const reportInterval = 10 * time.Second
+
+//Struct for metrics
+type Metrics struct {
+	Alloc         gauge
+	BuckHashSys   gauge
+	Frees         gauge
+	GCCPUFraction gauge
+	GCSys         gauge
+	HeapAlloc     gauge
+	HeapIdle      gauge
+	HeapInuse     gauge
+	HeapObjects   gauge
+	HeapReleased  gauge
+	HeapSys       gauge
+	LastGC        gauge
+	Lookups       gauge
+	MCacheInuse   gauge
+	MCacheSys     gauge
+	MSpanInuse    gauge
+	MSpanSys      gauge
+	Mallocs       gauge
+	NextGC        gauge
+	NumForcedGC   gauge
+	NumGC         gauge
+	OtherSys      gauge
+	PauseTotalNs  gauge
+	StackInuse    gauge
+	StackSys      gauge
+	Sys           gauge
+	TotalAlloc    gauge
+
+	PollCount   counter
+	RandomValue gauge
+}
+
+func main() {
+	M := Metrics{}
+	//init terminate
+	ctx, cancel := context.WithCancel(context.Background())
+	go waitSignals(cancel)
+
+	//set ticker dor sending
+	// tickerSend := time.NewTicker(reportInterval)
+	// defer tickerSend.Stop()
+	go UpdateMetrics(ctx, &M, pollInterval)
+	for {
+		select {
+		case <-ctx.Done():
+			logrus.Info("Agent is down send metrics.")
+			return
+		default:
+			time.Sleep(reportInterval)
+			err := sendMetrics(M)
+			if err != nil {
+				logrus.Error("Error sending POST: ", err)
+			}
+		}
+	}
+}
+
+//sending metrics to server
+func sendMetrics(M Metrics) error {
+	metricsMap := structs.Map(M)
+
+	client := resty.New()
+	client.
+		SetRetryCount(2).
+		SetRetryWaitTime(10 * time.Second).
+		SetRetryMaxWaitTime(20 * time.Second)
+
+	for k, v := range metricsMap {
+		typeMet := strings.Split(reflect.ValueOf(v).Type().String(), ".")[1]
+		value := fmt.Sprintf("%v", v)
+		_, err := client.R().SetPathParams(map[string]string{
+			"type": typeMet, "value": value, "name": k,
+		}).Post("http://127.0.0.1:8080/update/{type}/{name}/{value}")
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+	return nil
+}
+
+//wating signals
+func waitSignals(cancel context.CancelFunc) {
+	terminate := make(chan os.Signal)
+	signal.Notify(terminate, syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT)
+	for {
+		sig := <-terminate
+		switch sig {
+		case os.Interrupt:
+			logrus.Info("Agent is down terminate!")
+			cancel()
+			return
+		}
+	}
+}
+
+//Update metrics
+func UpdateMetrics(ctx context.Context, M *Metrics, pollInterval time.Duration) {
+	var memStats runtime.MemStats
+	//Init ticker
+	ticker := time.NewTicker(pollInterval)
+	defer ticker.Stop()
+	//init pollcount
+	PollCount := 0
+	for {
+		select {
+		//send command to ending
+		case <-ctx.Done():
+			logrus.Info("Agent is down update metrics!")
+			return
+		default:
+			<-ticker.C
+			runtime.ReadMemStats(&memStats)
+			//upper PollCount
+			PollCount++
+			// fmt.Println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!", PollCount)
+			//Update metrics
+			*M = Metrics{
+				Alloc:         gauge((memStats.Alloc)),
+				BuckHashSys:   gauge(memStats.BuckHashSys),
+				Frees:         gauge(memStats.Frees),
+				GCCPUFraction: gauge(memStats.GCCPUFraction),
+				GCSys:         gauge(memStats.GCSys),
+				HeapAlloc:     gauge(memStats.HeapAlloc),
+				HeapIdle:      gauge(memStats.HeapIdle),
+				HeapInuse:     gauge(memStats.HeapInuse),
+				HeapObjects:   gauge(memStats.HeapObjects),
+				HeapReleased:  gauge(memStats.HeapReleased),
+				HeapSys:       gauge(memStats.HeapSys),
+				LastGC:        gauge(memStats.LastGC),
+				Lookups:       gauge(memStats.Lookups),
+				MCacheInuse:   gauge(memStats.MCacheInuse),
+				MCacheSys:     gauge(memStats.MCacheSys),
+				MSpanInuse:    gauge(memStats.MSpanInuse),
+				MSpanSys:      gauge(memStats.MSpanSys),
+				Mallocs:       gauge(memStats.Mallocs),
+				NextGC:        gauge(memStats.NextGC),
+				NumForcedGC:   gauge(memStats.NumForcedGC),
+				NumGC:         gauge(memStats.NumGC),
+				OtherSys:      gauge(memStats.OtherSys),
+				PauseTotalNs:  gauge(memStats.PauseTotalNs),
+				StackInuse:    gauge(memStats.StackInuse),
+				StackSys:      gauge(memStats.StackSys),
+				Sys:           gauge(memStats.Sys),
+				TotalAlloc:    gauge(memStats.TotalAlloc),
+				RandomValue:   gauge(rand.Float64()),
+				PollCount:     counter(PollCount),
+			}
+		}
+	}
+}
---
+++ cmd/agent/main_test.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"context"
+	"math/rand"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var M Metrics
+
+func TestUpdateMetrics(t *testing.T) {
+
+	tests := []struct {
+		name    string
+		metrics *Metrics
+		want    *Metrics
+	}{
+		{
+			name: "first #",
+			metrics: &Metrics{
+				Alloc:         gauge(1.2),
+				BuckHashSys:   gauge(2.1),
+				Frees:         gauge(2.1),
+				GCCPUFraction: gauge(2.1),
+				GCSys:         gauge(2.1),
+				HeapAlloc:     gauge(0.00001),
+				HeapIdle:      gauge(3),
+				HeapInuse:     gauge(4),
+				HeapObjects:   gauge(5),
+				HeapReleased:  gauge(6),
+				HeapSys:       gauge(7),
+				LastGC:        gauge(8),
+				Lookups:       gauge(9),
+				MCacheInuse:   gauge(10),
+				MCacheSys:     gauge(11),
+				MSpanInuse:    gauge(12),
+				MSpanSys:      gauge(13),
+				Mallocs:       gauge(14),
+				NextGC:        gauge(15),
+				NumForcedGC:   gauge(16),
+				NumGC:         gauge(17),
+				OtherSys:      gauge(0),
+				PauseTotalNs:  gauge(0),
+				StackInuse:    gauge(0),
+				StackSys:      gauge(8),
+				Sys:           gauge(8),
+				TotalAlloc:    gauge(5),
+				RandomValue:   gauge(rand.Float64()),
+				PollCount:     counter(0),
+			},
+
+			want: &Metrics{
+				Alloc:         gauge(1.2),
+				BuckHashSys:   gauge(2.1),
+				Frees:         gauge(2.1),
+				GCCPUFraction: gauge(2.1),
+				GCSys:         gauge(2.1),
+				HeapAlloc:     gauge(0.00001),
+				HeapIdle:      gauge(3),
+				HeapInuse:     gauge(4),
+				HeapObjects:   gauge(5),
+				HeapReleased:  gauge(6),
+				HeapSys:       gauge(7),
+				LastGC:        gauge(8),
+				Lookups:       gauge(9),
+				MCacheInuse:   gauge(10),
+				MCacheSys:     gauge(11),
+				MSpanInuse:    gauge(12),
+				MSpanSys:      gauge(13),
+				Mallocs:       gauge(14),
+				NextGC:        gauge(15),
+				NumForcedGC:   gauge(16),
+				NumGC:         gauge(17),
+				OtherSys:      gauge(0),
+				PauseTotalNs:  gauge(0),
+				StackInuse:    gauge(0),
+				StackSys:      gauge(8),
+				Sys:           gauge(8),
+				TotalAlloc:    gauge(5),
+				RandomValue:   gauge(rand.Float64()),
+				PollCount:     counter(3),
+			},
+		},
+	}
+	for _, tt := range tests {
+		pollint := 2 * time.Second
+		ctx, cancel := context.WithCancel(context.Background())
+		time.AfterFunc(4*time.Second, cancel)
+		t.Run(tt.name, func(t *testing.T) {
+			UpdateMetrics(ctx, tt.metrics, pollint)
+			M = *tt.metrics
+			assert.Equal(t, tt.want.PollCount, M.PollCount)
+		})
+	}
+}
+
+// func Test_saveMetrics(t *testing.T) {
+
+// 	tests := []struct {
+// 		name    string
+// 		metrics Metrics
+// 	}{
+// 		{
+// 			name:    "first test",
+// 			metrics: M,
+// 		},
+// 	}
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			err := sendMetrics(tt.metrics)
+// 			require.Error(t, err)
+
+// 		})
+// 	}
+// }
