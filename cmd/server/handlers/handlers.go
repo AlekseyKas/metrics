6ced56c0152478e0a4d2fe43232a2a6b1404e64f
AlekseyKas <gos7Pride@yandex.ru>
2022-04-11 19:08:33 +0300

    2

--- cmd/server/handlers/handlers.go
+++ cmd/server/handlers/handlers.go
@@ -157,7 +157,7 @@ func saveMetricsJSON() http.HandlerFunc {
 		}
 		//update counter
 		if typeMet == "counter" {
-			fmt.Println("8888888888888888888888888888", nameMet, typeMet)
+			fmt.Println("8888888888888888888888888888", nameMet, typeMet, s.Delta)
 			var valueMetInt int
 			if err == nil {
 				if _, ok := metrics[nameMet]; ok {
