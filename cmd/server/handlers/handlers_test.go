d7d1d4790300a23d1278fcbb601c445e017dc4c1
AlekseyKas <gos7Pride@yandex.ru>
2022-04-18 00:31:08 +0300

    incr8.6

--- cmd/server/handlers/handlers_test.go
+++ cmd/server/handlers/handlers_test.go
@@ -59,7 +59,7 @@ func TestRouter(t *testing.T) {
 			body:   []byte(`{"ID": "PollCount", "type": "gouge"}`),
 			want: want{
 				contentType: "application/json",
-				statusCode:  200,
+				statusCode:  500,
 			},
 		},
 		{
@@ -69,7 +69,7 @@ func TestRouter(t *testing.T) {
 			body:   []byte(`{"ID": "MetricName", "type": "test"}`),
 			want: want{
 				contentType: "application/json",
-				statusCode:  200,
+				statusCode:  500,
 			},
 		},
 		{
@@ -89,7 +89,7 @@ func TestRouter(t *testing.T) {
 			body:   []byte(`{"ID": "Pollcount", "type": "gauge", "delta": "12"}`),
 			want: want{
 				contentType: "application/json",
-				statusCode:  200,
+				statusCode:  400,
 			},
 		},
 		{
