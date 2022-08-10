d0235ad0b26d4753f8244f47b03484f97e58ba9a
AlekseyKas <gos7Pride@yandex.ru>
2022-08-10 10:40:36 +0300

    add test for DB

--- cmd/server/database/database_test.go
+++ cmd/server/database/database_test.go
@@ -3,17 +3,15 @@ package database
 import (
 	"testing"
 
-	"github.com/AlekseyKas/metrics/internal/helpers"
 	"github.com/stretchr/testify/require"
 )
 
 func TestDBConnect(t *testing.T) {
-	DBURL, id, _ := helpers.StartDB()
+	DBURL := "postgres://postgres:postgres@postgres:5432/praktikum?sslmode=disable"
 	t.Run("Test DB connect", func(t *testing.T) {
 		err := DBConnect(DBURL)
 		require.NoError(t, err)
 	})
-	helpers.StopDB(id)
 }
 
 func TestDBClose(t *testing.T) {
