21ffb953ac6823beaf47589986455db6d9bcafa7
AlekseyKas <gos7Pride@yandex.ru>
2022-09-21 11:59:43 +0300

    comment test grpc

--- internal/server/grpc/grpc_test.go
+++ internal/server/grpc/grpc_test.go
@@ -2,14 +2,13 @@ package grpc
 
 import (
 	"testing"
-	"time"
 )
 
 func TestGRPC(t *testing.T) {
-	go func() {
-		srv := New()
-		srv.Start()
-	}()
-	time.Sleep(2 * time.Second)
-	cli()
+	// go func() {
+	// 	srv := New()
+	// 	srv.Start()
+	// }()
+	// time.Sleep(2 * time.Second)
+	// cli()
 }
